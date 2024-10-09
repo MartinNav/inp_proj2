4799d1313a84ad1e68e351965d317aea13b91b72
MartinNav <martinnavratilu@gmail.com>
2024-10-09 20:07:04 +0200

    wrote some part of logic but there are syntactical errors

--- Projekt2/hello.s
+++ Projekt2/hello.s
@@ -18,11 +18,29 @@ params_sys5:    .space  8 ; misto pro ulozeni adresy pocatku
                 .text
 
 main:           ; ZDE NAHRADTE KOD VASIM RESENIM
+                ; make sure r0 contains zero
+                xor     r0, r0, r0
+                ;first letter of surname
+                addi    r8, r0, msg
+                addi    r8, r8, 16 
+                ;letter offset
+                addi    r7, r0, 7
+                ;prepare for an aray len
                 addi    r1, r0, 30
                 enc_loop:
                 ;here will be all the instructions
+                ;load the ciphertext array
+                lbu     r10, r1(cipher); this is going from back and we may want to go from front
+                ;load key char
+                lbu     r9, r8(r7)
+                ;prepare offset for
+                addi    r7, r7, 1
+                and     r7, r7, 3
+
+                ;end loop
                 subi r1, r1, 1
                 bne     r1, r0, enc_loop
+
                 daddi   r4, r0, msg ; vozrovy vypis: adresa msg do r4
                 jal     print_string ; vypis pomoci print_string - viz nize
 
