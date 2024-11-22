928b0572346434271574b1b4efb09bb0507b8ede
MartinNav <martinnavratilu@gmail.com>
2024-11-22 13:53:42 +0100

    added key creation logic

--- Projekt2/hello.s
+++ Projekt2/hello.s
@@ -9,6 +9,7 @@ msg:            .asciiz "martinnavratil" ; sem doplnte vase "jmenoprijmeni"
 cipher:         .space  31 ; misto pro zapis zasifrovaneho textu
 ; zde si muzete nadefinovat vlastni promenne ci konstanty,
 ; napr. hodnoty posuvu pro jednotlive znaky sifrovacho klice
+key:            .asciiz "xxx"
 
 params_sys5:    .space  8 ; misto pro ulozeni adresy pocatku
                           ; retezce pro vypis pomoci syscall 5
@@ -19,27 +20,23 @@ params_sys5:    .space  8 ; misto pro ulozeni adresy pocatku
 
 main:           ; ZDE NAHRADTE KOD VASIM RESENIM
                 ; make sure r0 contains zero
-                xor     r0, r0, $0
-                ;first letter of surname
-                addi    r8, r0, msg
-                addi    r8, r8, 16 
-                ;letter offset
-                addi    r7, r0, 7
-                ;prepare for an aray len
-                addi    r11, r0, 30
-                enc_loop:
-                ;here will be all the instructions
-                ;load the ciphertext array
-                lbu     r10, r11(cipher); this is going from back and we may want to go from front
-                ;load key char
-                lbu     r9, r8(r7)
-                ;prepare offset for
-                addi    r12, r12, 1
-                and     r12, r12, 3
+                xor     r0, r0, r0
+                ;copy/create key
+                ;copy 'n'
+                addi     r1, r0, 6
+                lb      r2, msg(r1)
+                sb r2, key(r0)
+                ;copy 'a'
+                addi     r1, r1, 1
+                lb      r2, msg(r1)
+                addi     r3, r0, 1
+                sb      r2, key(r3)
+                ;copy 'v'
+                addi     r1, r1, 1
+                lb      r2, msg(r1)
+                addi     r3, r3, 1
+                sb      r2, key(r3)
 
-                ;end loop
-                subi r11, r11, 1
-                bne     r11, r0, enc_loop
 
                 daddi   r4, r0, msg ; vozrovy vypis: adresa msg do r4
                 jal     print_string ; vypis pomoci print_string - viz nize
