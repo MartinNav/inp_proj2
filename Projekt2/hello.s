de2a6f633c740361fac23b5e5c7edf32e8b43213
MartinNav <martinnavratilu@gmail.com>
2024-11-29 10:53:15 +0100

    started doing rewrite

---
+++ Projekt2/hello.s
@@ -0,0 +1,43 @@
+; Autor reseni: jmeno prijmeni login
+
+; Projekt 2 - INP 2024
+; Vigenerova sifra na architekture MIPS64
+
+; DATA SEGMENT
+                .data
+msg:            .asciiz "martinnavratil" ; sem doplnte vase "jmenoprijmeni"
+cipher:         .space  31 ; misto pro zapis zasifrovaneho textu
+; zde si muzete nadefinovat vlastni promenne ci konstanty,
+; napr. hodnoty posuvu pro jednotlive znaky sifrovacho klice
+key:            .asciiz "nav"
+
+params_sys5:    .space  8 ; misto pro ulozeni adresy pocatku
+                          ; retezce pro vypis pomoci syscall 5
+                          ; (viz nize "funkce" print_string)
+
+; CODE SEGMENT
+                .text
+
+main:           ; ZDE NAHRADTE KOD VASIM RESENIM
+                addi r1, r0, 0;index of an message
+start_of_loop:  
+                lb r2, cipher(r1); r2 will contain the unencripted message
+                beqz r2, end_of_loop; in case we hit null byte we will end the loop
+
+                addi r1, r1, 1; will move to the next character
+                b start_of_loop; and repeat it again
+
+end_of_loop:
+                daddi   r4, r0, msg ; vozrovy vypis: adresa msg do r4
+                jal     print_string ; vypis pomoci print_string - viz nize
+
+
+; NASLEDUJICI KOD NEMODIFIKUJTE!
+
+                syscall 0   ; halt
+
+print_string:   ; adresa retezce se ocekava v r4
+                sw      r4, params_sys5(r0)
+                daddi   r14, r0, params_sys5    ; adr pro syscall 5 musi do r14
+                syscall 5   ; systemova procedura - vypis retezce na terminal
+                jr      r31 ; return - r31 je urcen na return address
